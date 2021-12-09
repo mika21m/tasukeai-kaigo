99f466933f4c63f5b1f5b9d9c781012530f0348e
mika21m <93243570+mika21m@users.noreply.github.com>
2021-12-09 16:42:18 +0900

    paramsにimage追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:text).merge(user_id: current_user.id)
+    params.require(:post).permit(:text, :image).merge(user_id: current_user.id)
   end
   
 end
