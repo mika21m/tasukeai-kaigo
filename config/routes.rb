4e55b577d945b825b108af89cfde6e7ff5d50c8f
mika21m <93243570+mika21m@users.noreply.github.com>
2021-12-10 15:48:57 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts, only: [:index, :new, :create, :destroy, :edit, :update]
+  resources :users, only: :show
 end
